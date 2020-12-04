0492e913c39ddd16665d7e4d0ba21b20e7b9727c
patrickryanpo <73608741+patrickryanpo@users.noreply.github.com>
2020-12-04 14:12:57 +0800

    Update Readme Challenge File and JPG

--- Practice:Class Files/Practice2.py
+++ Practice:Class Files/Practice2.py
@@ -1,13 +1,15 @@
 
-count = 0
-while count < 1:
-    print("Hello World")
-    count = count + 0.5
+# count = 0
+# while count < 1:
+#     print("Hello World")
+#     count = count + 0.5
 
 voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
-for county, voters in voting_data.items():
+
+for vote_count in voting_data:
 #print(f"{voting_data[0]['county']} county has {voting_data[0]['registered_voters']} registered voters.")
 #print(f"{voting_data[1]['county']} county has {voting_data[1]['registered_voters']} registered voters.")
 #print(f"{voting_data[2]['county']} county has {voting_data[2]['registered_voters']} registered voters.")
-#for county, registered_voters in voting_data
-    print(f'{voting_data[county]} has {voting_data[registered_voters]}.')
+#for county, registered_voters in voting_data:
+
+    print(f"{vote_count['county']} has {vote_count['registered_voters']:,} registered voters.")
